99b960475de9f45218b28012ef2fc247a8782b11
AlaCrity <wlchen1105@gmail.com>
2019-06-29 11:02:18 -0400

    linked the book and movie API to the search results form

--- js/index.js
+++ js/index.js
@@ -1,20 +1,29 @@
+function showModal() {
+  $("#myModal").modal("show");
+}
+
+function showSearchResults(e) {
+  //   $("form").after(
+  //     '<div id="showSearchResults" style="background: white">show results here</div>'
+  //   );
+  //   e.preventDefault();
+}
+
 $("#form").submit(function(event) {
+  console.log("IM RUNNING");
   event.preventDefault();
+
+  $("#form").append(
+    '<div id="showSearchResults" style="background: white"></div>'
+  );
 
   let search = $("#input")
     .val()
     .trim();
 
   bookInfo(search);
+  $("#showSearchResults").append("<div id='gap'>");
   movieInfo(search);
 });
 
-function showModal() {
-  $("#myModal").modal("show");
-}
-
-function showSearchResults(e) {
-  $("form").after('<div style="background: white">show results here</div>');
-  e.preventDefault();
-}
 // $('.parallax-window').parallax({imageSrc: 'bookbackground.jpg'});
